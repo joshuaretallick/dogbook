3f81cb05eee7a5a733cbc1c6dab77e96090ea074
Samantha <samanthagottlieb31@gmail.com>
2021-01-04 16:43:38 +0000

    Redirect unathenticated users to login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   get 'pages/info'
-  root to: redirect('/posts')
+  root to: redirect('/users/sign_up')
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
