fb5ef1590d7e0f465442c9eaaf43c4587a6805a7
Joshua Retallick <josh@old-empire.co.uk>
2021-01-04 14:36:54 +0000

    Added first test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_120518) do
+ActiveRecord::Schema.define(version: 2021_01_04_133818) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_120518) do
     t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
