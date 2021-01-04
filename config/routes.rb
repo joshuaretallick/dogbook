1490d32a34ef54f77dba513a689897f0ecb3dd1d
KAB <>
2021-01-04 17:52:54 +0000

    Change redirect to posts
    Fix redirect loop by changing root to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  # root to: redirect('/users/sign_up')
   devise_for :users
   resources :comments
+  root to: redirect('/posts')
   get 'pages/info'
-  root to: redirect('/users/sign_up')
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
