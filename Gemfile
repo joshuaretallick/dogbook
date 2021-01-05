ddf4060bc2ad2a901adf5e57f4e01877309b1dbb
Joshua Retallick <josh@old-empire.co.uk>
2021-01-05 17:35:15 +0000

    Added Factory Bot gem, created devise factory to create fake user, then login macro that can run in test.  Finally a post controller test using said macro which logs user in then tests if they can post content

--- Gemfile
+++ Gemfile
@@ -66,6 +66,7 @@ group :test do
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
   gem 'simplecov', require: false
+  gem 'factory_bot_rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+
+  login_user
+
+  let(:valid_attributes) {
+      { :description => "This is a test description" }
+  }
+
+  let(:valid_session) { {} }
+
+  describe "GET #index" do
+      it "returns a success response" do
+          Post.create! valid_attributes
+          get :index, params: {}, session: valid_session
+          expect(response).to be_successful # be_successful expects a HTTP Status code of 200
+          # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
+      end
+  end
+
+
   describe 'GET /new ' do
     xit 'responds with 200' do
       get :new
