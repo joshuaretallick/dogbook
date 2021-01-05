48da20c0ce3f913081ddfbfe3006ddedf259a7f8
Joshua Retallick <josh@old-empire.co.uk>
2021-01-05 17:16:01 +0000

    Created comment feature testing to check user can add, edit and delete comments

--- spec/features/creating_post_spec.rb
+++ spec/features/creating_post_spec.rb
@@ -53,4 +53,20 @@ feature 'post creation' do
     expect(page).to have_content('Post was successfully updated.')
   end
 
+  it 'lets the user edit a post signed in' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'hyko@example.com'
+    fill_in 'Name', with: 'Hyko'
+    fill_in 'Password', with: 'Squirrels'
+    fill_in 'Password confirmation', with: 'Squirrels'
+    click_button 'Sign up'
+    click_link 'New Post'
+    fill_in 'Description', with: 'I love chasing squirrels'
+    page.attach_file(Rails.root + "spec/images/dog_park.jpeg")
+    click_button('Create Post')
+    click_link 'Destroy'
+    expect(page).to have_content('Post was successfully destroyed.')
+  end
+
 end
