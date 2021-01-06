2dcc65fea9a790b0e253755e41f4a69c22a36a12
savannaelbey <savanna_e@icloud.com>
2021-01-06 16:29:21 +0000

    test: rename and pass previous tests, incomplete

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -1,60 +1,57 @@
 require 'rails_helper.rb'
 
 feature 'comment creation' do
-  it 'lets the user create a comment when signed in' do
+  it 'lets the user create comment on their own post when signed in' do
     visit '/'
-    click_link 'Sign up'
+    click_link 'Join pack'
     fill_in 'Email', with: 'hyko@example.com'
     fill_in 'Name', with: 'Hyko'
     fill_in 'Password', with: 'Squirrels'
     fill_in 'Password confirmation', with: 'Squirrels'
-    click_button 'Sign up'
-    click_link 'New Post'
+    click_button 'Join pack'
+    click_link 'New bark'
     fill_in 'Description', with: 'I love chasing squirrels'
-    click_button 'Create Post'
+    click_button 'Bark!'
     fill_in 'Body', with: 'I love chasing squirrels too!'
-    click_button 'Create Comment'
-    expect(page).to have_content('Comment was successfully created.')
+    click_button 'Sniff!'
+    expect(page).to have_content('Sniff was sockcessful')
   end
 
-  it 'lets the user edit a comment when signed in' do
+  it 'lets the user edit a comment they made on their own post when signed in' do
     visit '/'
-    click_link 'Sign up'
+    click_link 'Join pack'
     fill_in 'Email', with: 'hyko@example.com'
     fill_in 'Name', with: 'Hyko'
     fill_in 'Password', with: 'Squirrels'
     fill_in 'Password confirmation', with: 'Squirrels'
-    click_button 'Sign up'
-    click_link 'New Post'
+    click_button 'Join pack'
+    click_link 'New bark'
     fill_in 'Description', with: 'I love chasing squirrels'
-    click_button 'Create Post'
+    click_button 'Bark!'
     fill_in 'Body', with: 'I love chasing squirrels too!'
-    click_button 'Create Comment'
+    click_button 'Sniff!'
     click_link 'Edit'
     fill_in 'Body', with: 'I really love chasing squirrels too!'
-    click_button 'Update Comment'
-    expect(page).to have_content('Comment was successfully updated.')
+    click_button 'Sniff'
+    expect(page).to have_content('Sniff pupdated')
   end
 
-  it 'lets the user delete a comment when signed in' do
+  it 'lets the user delete their comment when signed in' do
     visit '/'
-    click_link 'Sign up'
+    click_link 'Join pack'
     fill_in 'Email', with: 'hyko@example.com'
     fill_in 'Name', with: 'Hyko'
     fill_in 'Password', with: 'Squirrels'
     fill_in 'Password confirmation', with: 'Squirrels'
-    click_button 'Sign up'
-    click_link 'New Post'
+    click_button 'Join pack'
+    click_link 'New bark'
     fill_in 'Description', with: 'I love chasing squirrels'
-    click_button 'Create Post'
+    click_button 'Bark!'
     fill_in 'Body', with: 'I love chasing squirrels too!'
-    click_button 'Create Comment'
-    click_link 'Edit'
-    fill_in 'Body', with: 'I really love chasing squirrels too!'
-    click_button 'Update Comment'
-    click_link 'Home'
+    click_button 'Sniff!'
+    click_link 'Park'
     click_link 'I love chasing squirrels'
     click_link 'Delete'
-    expect(page).to have_content('Comment was successfully destroyed.')
+    expect(page).to have_content('Nothing to be sniffed at here')
   end
 end
