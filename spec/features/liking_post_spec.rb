27629c9a92a95aeaa6cce0d91ab9ff2621728d48
savannaelbey <savanna_e@icloud.com>
2021-01-08 12:32:06 +0000

    add 'lets users unlike posts' feature tests

--- spec/features/liking_post_spec.rb
+++ spec/features/liking_post_spec.rb
@@ -18,5 +18,24 @@ feature 'liking posts' do
     expect(page).to have_content('1 Lick')
   end
 
+  it 'lets users unlike posts' do
+    sign_up
+    post_photo_bark
+    click_link 'Logout'
+    user2_sign_up
+    click_link 'I love chasing squirrels'
+    click_button 'Lick'
+    expect(page).to have_content('1 Lick')
+    click_button 'Unlick'
+    expect(page).to have_content('0 Licks')
+    click_link 'New bark'
+    post_photo_bark
+    click_button 'Lick'
+    expect(page).to have_content('1 Lick')
+    click_button 'Unlick'
+    expect(page).to have_content('0 Licks')
+
+  end
+
 
 end
