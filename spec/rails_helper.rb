c6dc8b61449334142ba9a892ad3aea32cae5beb5
Joshua Retallick <josh@old-empire.co.uk>
2021-01-08 11:42:15 +0000

    Edit app.js to import style.scss

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test' ./bin/rails webpacker:compile
+ENV['RAILS_ENV'] ||= 'test' 
 
 require File.expand_path('../config/environment', __dir__)
 
